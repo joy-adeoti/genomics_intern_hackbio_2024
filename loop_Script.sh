4830e2d828c86c4d1acb072fcad32ec090c75932
joy-adeoti <joyadeoti2@gmail.com>
2024-04-05 11:52:28 +0800

    Update loop_Script.sh

--- loop_Script.sh
+++ loop_Script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #loop_script for ngs pipeline
 mkdir -p data
-data_dir="data"
+Data_dir="data"
 SAMPLES=(
     "ACBarrie"
     "Alsen"
@@ -15,36 +15,36 @@ for sample in "${SAMPLES[@]}";do
     read1_url="https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/${sample}_R1.fastq.gz"
     read2_url="https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/${sample}_R2.fastq.gz"
     
-    wget -P "${data_dir}" "$read1_url"
-    wget -P "${data_dir}" "$read2_url" 
+    wget -P "${Data_dir}" "$read1_url"
+    wget -P "${Data_dir}" "$read2_url" 
 done
 
 ref_url="https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/raw_reads/reference.fasta"
-wget -P "${data_dir}" "$ref_url"
+wget -P "${Data_dir}" "$ref_url"
 
 #to perform the quality control using fastqc
 mkdir -p fastqc_out
-for fq in "${data_dir}"/*.fastq.gz;do
+for fq in "${Data_dir}"/*.fastq.gz;do
     #fastqc -i "${fq}" -o fastqc_out
     fastqc -f fastq -o fastqc_out "${fq}"
 done
 
 #to perform trimming using the fastp
 for fastq in "${SAMPLES[@]}";do
-    read1="${data_dir}/${fastq}_R1.fastq.gz"
-    read1_out="${data_dir}/${fastq}_R1.trimmed.fastq.gz"
-    read2="${data_dir}/${fastq}_R2.fastq.gz"
-    read2_out="${data_dir}/${fastq}_R2.trimmed.fastq.gz"
+    read1="${Data_dir}/${fastq}_R1.fastq.gz"
+    read1_out="${Data_dir}/${fastq}_R1.trimmed.fastq.gz"
+    read2="${Data_dir}/${fastq}_R2.fastq.gz"
+    read2_out="${Data_dir}/${fastq}_R2.trimmed.fastq.gz"
     fastp -i "${read1}" -o "${read1_out}" -I "${read2}" -O "${read2_out}"
 done
 
 #to perform genome mapping with the reference fasta using bwa
-reference="${data_dir}/reference.fasta"
+reference="${Data_dir}/reference.fasta"
 bwa index -a bwtsw "${reference}"
 mkdir -p bwa_outputfile
 for map in "${SAMPLES[@]}"; do
-    read1="${data_dir}/${map}_R1.trimmed.fastq.gz"
-    read2="${data_dir}/${map}_R2.fastq.gz"
+    read1="${Data_dir}/${map}_R1.trimmed.fastq.gz"
+    read2="${Data_dir}/${map}_R2.fastq.gz"
     bwa_result="bwa_outputfile/${map}.sam"
     bwa mem "${reference}" "${read1}" "${read2}" > "${bwa_result}"
     samtools view -hbo "bwa_outputfile/${map}.bam" "bwa_outputfile/${map}.sam"
